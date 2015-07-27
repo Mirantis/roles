7db117f776dcb2ed941c02d2fac24dff6fc25612
Scott Brimhall <sbrimhall@mirantis.com>
2015-07-27 12:54:11 -0700

    Add roles for pmlc versions of openstack boxen

---
+++ manifests/openstack/pmlc_ceph_osd.pp
@@ -0,0 +1,34 @@
+# == Class: roles::openstack::pmlc_ceph_osd
+#
+# This is a role class. It is to be assigned (1 per server)
+# to a system as its classification on what it does.
+# Role classes only include profile classes. No org-specific
+# lookups or anything like that. Includes only.
+#
+# This particular role is for a Ceph OSD node using the
+# masterless preliminary mirantis lifecycle management.
+#
+# === Authors
+#
+# Scott Brimhall <sbrimhall@mirantis.com>
+#
+# === Copyright
+#
+# Copyright 2015 Mirantis Inc, unless otherwise noted.
+#
+class roles::openstack::pmlc_ceph_osd {
+
+  include ::profiles::ntp::client
+  include ::profiles::common::users
+  include ::profiles::common::sudo
+  include ::profiles::common::packages
+  include ::profiles::ssh::server
+
+  # Order classes in the order we want them applied
+  Class['::profiles::ntp::client'] ->
+  Class['::profiles::common::users'] ->
+  Class['::profiles::common::sudo'] ->
+  Class['::profiles::common::packages'] ->
+  Class['::profiles::ssh::server']
+
+}
---
+++ manifests/openstack/pmlc_compute.pp
@@ -0,0 +1,39 @@
+# == Class: roles::openstack::pmlc_compute
+#
+# This is a role class. It is to be assigned (1 per server)
+# to a system as its classification on what it does.
+# Role classes only include profile classes. No org-specific
+# lookups or anything like that. Includes only.
+#
+# This particular role is for an openstack compute node
+# using the masterless preliminary mirantis lifecycle management.
+#
+# === Authors
+#
+# Scott Brimhall <sbrimhall@mirantis.com>
+#
+# === Copyright
+#
+# Copyright 2015 Mirantis Inc, unless otherwise noted.
+#
+class roles::openstack::pmlc_compute {
+
+  include ::profiles::ntp::client
+  include ::profiles::common::users
+  include ::profiles::common::sudo
+  include ::profiles::common::packages
+  include ::profiles::ssh::server
+  include ::profiles::openstack::nova::compute
+  include ::profiles::openstack::neutron::client
+
+  # Order classes in the order we want them applied
+  Class['::profiles::ntp::client'] ->
+  Class['::profiles::common::users'] ->
+  Class['::profiles::common::sudo'] ->
+  Class['::profiles::common::packages'] ->
+  Class['::profiles::ssh::server'] ->
+  Class['::profiles::openstack::keystone'] ->
+  Class['::profiles::openstack::nova::compute'] ->
+  Class['::profiles::openstack::neutron::client']
+
+}
