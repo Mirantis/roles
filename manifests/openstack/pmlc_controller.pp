5b94f457643cce0d6d2bee70deabfa18bfe229fa
Scott Brimhall <sbrimhall@mirantis.com>
2015-07-27 19:20:12 -0700

    Revert "debug sudo"
    This reverts commit e015defa639aaaaeda428c47e3381cd350985a6e.

--- manifests/openstack/pmlc_controller.pp
+++ manifests/openstack/pmlc_controller.pp
@@ -20,6 +20,7 @@ class roles::openstack::pmlc_controller {
 
   include ::profiles::ntp::client
   include ::profiles::common::users
+  include ::profiles::common::sudo
   include ::profiles::common::packages
   include ::profiles::ssh::server
   include ::profiles::openstack::keystone
@@ -30,6 +31,7 @@ class roles::openstack::pmlc_controller {
   # Order classes in the order we want them applied
   Class['::profiles::ntp::client'] ->
   Class['::profiles::common::users'] ->
+  Class['::profiles::common::sudo'] ->
   Class['::profiles::common::packages'] ->
   Class['::profiles::ssh::server'] ->
   Class['::profiles::openstack::keystone'] ->
